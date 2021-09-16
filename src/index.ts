23326a8492b91a3e39e72c2fbbec14064a4e00cc
sunchenlong111 <1094584419@qq.com>
2021-09-16 21:14:37 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ createConnection().then(async connection => {
 
   console.log(connection)
   connection.close()
-  console.log('test git2')
+  console.log('test git3')
 }).catch(error => console.log(error));
